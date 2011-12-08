f13de0274391828d1b47f9a4db9d2c141a5c7b8c
Joe Ferner <joe@fernsroth.com>
2011-12-08 12:39:17 -0500

    working select

--- tests/integration/delete.js
+++ tests/integration/delete.js
@@ -6,27 +6,39 @@ exports['Delete'] = nodeunit.testCase({
   setUp: function(callback) {
     var self = this;
 
-    testUtils.connect(persist, function(connection) {
+    testUtils.connect(persist, function(err, connection) {
       self.connection = connection;
+      self.connection.runSql("CREATE TABLE Person (id INTEGER PRIMARY KEY, name string);", function() {
+        self.Person = persist.define("Person", {
+          "name": "string"
+        });
 
-      self.Person = persist.define("Person", {
-        "name": "string"
-      });
-
-      self.person1 = new self.Person({ name: "bob" });
-      self.person2 = new self.Person({ name: "john" });
-      self.person1.save(self.connection, function(err, p1) {
-        self.person2.save(self.connection, function(err, p2) {
-          callback();
+        self.person1 = new self.Person({ name: "bob" });
+        self.person2 = new self.Person({ name: "john" });
+        self.person1.save(self.connection, function(err, p1) {
+          self.person2.save(self.connection, function(err, p2) {
+            callback();
+          });
         });
       });
     });
   },
 
+  tearDown: function(callback) {
+    this.connection.close();
+    callback();
+  },
+
   "delete with no associations": function(test) {
+    var self = this;
     this.person1.delete(function(err, deletedPerson) {
       test.ifError(err);
-      test.done();
+      self.Person.using(self.connection).all(function(err, rows) {
+        test.ifError(err);
+        test.equal(rows.length, 1);
+        test.equal(rows[0].name, 'john');
+        test.done();
+      });
     });
   },
 
--- tests/integration/select.js
+++ tests/integration/select.js
@@ -15,16 +15,35 @@ exports['Select'] = nodeunit.testCase({
       "name": "string"
     }).hasMany(this.Phone);
 
-    testUtils.connect(persist, function(connection) {
+    testUtils.connect(persist, function(err, connection) {
       self.connection = connection;
-      callback();
+      self.connection.runSql("CREATE TABLE Person (id INTEGER PRIMARY KEY, name string);", function() {
+        self.Person = persist.define("Person", {
+          "name": "string"
+        });
+
+        self.person1 = new self.Person({ name: "bob" });
+        self.person2 = new self.Person({ name: "john" });
+        self.person1.save(self.connection, function(err, p1) {
+          self.person2.save(self.connection, function(err, p2) {
+            callback();
+          });
+        });
+      });
     });
+  },
+
+  tearDown: function(callback) {
+    this.connection.close();
+    callback();
   },
 
   "all": function(test) {
     this.Person.using(this.connection).all(function(err, people) {
       test.ifError(err);
       test.equals(people.length, 2);
+      test.equals(people[0].name, 'bob');
+      test.equals(people[1].name, 'john');
 
       test.done();
     });
