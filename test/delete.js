cd9a688c17d80102feed941487ce799763986482
Joe Ferner <joe@fernsroth.com>
2012-04-19 13:52:41 -0400

    more tests

--- test/delete.js
+++ test/delete.js
@@ -4,52 +4,85 @@ var nodeunit = require("nodeunit");
 var testUtils = require("../test_helpers/test_utils");
 
 exports['Delete'] = nodeunit.testCase({
-  setUp: function(callback) {
+  setUp: function (callback) {
     var self = this;
 
-    testUtils.connect(persist, function(err, connection) {
+    testUtils.connect(persist, function (err, connection) {
       self.connection = connection;
       self.Person = persist.define("Person", {
         "name": type.STRING
       });
 
-      self.person1 = new self.Person({ name: "bob" });
-      self.person2 = new self.Person({ name: "john" });
-      self.person1.save(self.connection, function(err, p1) {
-        self.person2.save(self.connection, function(err, p2) {
-          callback();
-        });
+      self.person1 = new self.Person({ name: "Bob Smith" });
+      self.person2 = new self.Person({ name: "Joe Blow" });
+      self.person3 = new self.Person({ name: "Joe Smith" });
+      self.connection.save([self.person1, self.person2, self.person3], function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        callback();
       });
     });
   },
 
-  tearDown: function(callback) {
+  tearDown: function (callback) {
     this.connection.close();
     callback();
   },
 
-  "delete with no associations": function(test) {
+  "delete with no associations": function (test) {
     var self = this;
-    this.person1.delete(self.connection, function(err, deletedPerson) {
-      if(err) { console.log(err); return; }
-      self.Person.using(self.connection).all(function(err, rows) {
-        if(err) { console.log(err); return; }
-        test.equal(rows.length, 1);
-        test.equal(rows[0].name, 'john');
+    this.person1.delete(self.connection, function (err, deletedPerson) {
+      if (err) {
+        return console.log(err);
+      }
+      self.Person.orderBy("name").all(self.connection, function (err, rows) {
+        if (err) {
+          return console.log(err);
+        }
+        test.equal(rows.length, 2);
+        test.equal(rows[0].name, 'Joe Blow');
+        test.equal(rows[1].name, 'Joe Smith');
         test.done();
       });
     });
   },
 
-  "delete all of a certain type": function(test) {
+  "delete all of a certain type": function (test) {
     var self = this;
-    this.Person.using(this.connection).deleteAll(function(err) {
-      if(err) { console.log(err); return; }
-      self.Person.using(self.connection).all(function(err, rows) {
-        if(err) { console.log(err); return; }
+    this.Person.deleteAll(this.connection, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      self.Person.orderBy("name").all(self.connection, function (err, rows) {
+        if (err) {
+          return console.log(err);
+        }
         test.equal(rows.length, 0);
         test.done();
       });
     })
+  },
+
+  "delete all with query": function (test) {
+    var self = this;
+
+    self.Person.where("name LIKE ?", "Joe%").deleteAll(self.connection, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      self.Person.orderBy("name").all(self.connection, function (err, results) {
+        if (err) {
+          return console.log(err);
+        }
+
+        test.equals(1, results.length);
+
+        test.equals("Bob Smith", results[0].name);
+
+        test.done();
+      });
+    });
   }
 });
